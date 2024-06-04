93405fd75c04bdc329f930e0dc6a9af6f9632464
Vincenzo Sammartano <v.sammartano@gmail.com>
2024-06-04 21:07:59 +0200

    automatic procedure

--- pressurecoeff.py
+++ pressurecoeff.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jul 26 11:53:08 2018
 
-@author: bmusammartanoV
+@author: sammav
 """
 
 import matplotlib.pyplot as plt
@@ -16,15 +15,15 @@ def clearScreen():
  os.system("reset -f")
  clearScreen()
 
-def eq_fit(n,x,y,nv):
+def eq_fit(n,x,y,nv,N):
     s = "" #equation string
-    N = 6
     coeff = np.polyfit(x, y, N)
     Y = np.poly1d(coeff)
 
 ##    axs[nv].plot(x,Y(x),"ob")
 ##    axs[nv].plot(x,y,"--r")
 ##    axs[nv].set_title(n)
+    
     axs.set_title(n)
     axs.plot(x,Y(x),"ob")
     axs.plot(x,y,"--r")
@@ -32,43 +31,61 @@ def eq_fit(n,x,y,nv):
     plt.xlabel("Velocity, m/s")
     plt.ylabel("Loss Coeff, 1/m")
     
-    RMS_error = np.sqrt(np.mean(np.square(np.subtract(y,Y(x)))))
 
-    #x[k]**n * p[0] + ... + x[k] * p[n-1] + p[n] = y[k]
+    RMS_error = (np.square(np.subtract(y,Y(x))).mean())**.5
+
     
-    s0 = "IF(VelocityMagnitude < " + str(x[0]) + " [m/s] , " + str(y[0]) + " [1/m] ,"
-    for i in range(0,N+1):
-        s="+VelocityMagnitude^"+str(N-i)+ "*" + str(coeff[i])+" [m^"+str(-(N-i+1))+"s^"+str((N-i))+"]"+s
+    #string creation for Fluent
+    #x[k]**n * p[0] + ... + x[k] * p[n-1] + p[n] = y[k]
 
-    s_full = s0 + s + " , " + "IF(VelocityMagnitude > " + str(x[-1])  + " [m/s] , " + str(y[-1]) + " [1/m]  ))"
- 
+
+    #first part
+    s0 = "IF(VelocityMagnitude < " + str(x[0]) + " [m/s] , " + str(y[0]) + " [1/m] , "
+
+    #second part   
+    for i in range(0,N+1):
+        s = " +VelocityMagnitude^" + str(N-i) + " * " + '{:.1f}'.format(coeff[i]) + " [m^" + str(-(N-i+1)) + "s^" + str((N-i)) + " ] " + s
+
+    #third part    
+    s_full = s0 + " IF(VelocityMagnitude > " + str(x[-1])  + " [m/s] , " + str(y[-1]) + " [1/m] , " + s + ") )"
+
     print("\nThe equation for ",n," =\n", s_full )
     print("\nRoot mean square Error = ", RMS_error)
-    fig.subplots_adjust(hspace=0.5) 
-    return s, RMS_error
+    
+    fig.subplots_adjust(hspace = 0.5)
+    
+    return RMS_error
 
-
-#data acquisition
-file = "dp.txt" #name of the file to be opened 
-fid = open(file,"r+")
-velocity = []
-dp = []
-
-#Data from file
-next(fid) #skip the header file
-for line in fid:
-    line = line.split("\t") #remove the commas
-    velocity.append(float(line[0])) 
-    dp.append(float(line[1]))
-fid.close()
-
-#Data
-Vel = np.asarray(velocity)
-DP  = np.asarray(dp)
-#fig, axs = plt.subplots(2,figsize=(10,10))
-fig, axs = plt.subplots()
 
 if __name__ == '__main__':
-    eq_fit("Loss Coefficient",Vel, DP,0)
+    
+    #data acquisition
+    file = "dp.txt" #name of the file to be opened 
+    fid = open(file,"r+")
+    velocity = []
+    dp = []
+
+    #Data from file
+    for line in fid:
+        line = line.split("\t") #remove the commas
+        velocity.append(float(line[0])) 
+        dp.append(float(line[1]))
+    fid.close()
+
+
+    #Data
+    Vel = np.asarray(velocity)
+    DP  = np.asarray(dp)
+    #fig, axs = plt.subplots(2,figsize=(10,10))
+    fig, axs = plt.subplots(figsize=(10,5))
+
+
+    #function call
+    pol = 4
+    RMS_error = 1
+    while RMS_error > 0.05:
+        RMS_error = eq_fit("Loss Coefficient", Vel, DP, 0, pol)
+        pol += 1
+    print(pol)
     
     plt.show()
